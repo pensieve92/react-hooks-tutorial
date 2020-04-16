1798771ea0a5df19801ade0d53f67331af208766
JeonBH <pensieve92@gmail.com>
2020-04-16 11:21:49 +0900

    useEffect hook 4

--- hooks-totorial/src/App.js
+++ hooks-totorial/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from  './Counter';
 import Info from './Info';
 
 
 
 const App = () => {
+  const [visible, setVisible] = useState(false);
   // return <Counter />;
-  return <Info />;
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? '숨기기': '보이기'}
+      </button>
+      <hr />
+      {visible && <Info/>}
+    </div>    
+  )
+
+  
+  
 }
 
 export default App;
