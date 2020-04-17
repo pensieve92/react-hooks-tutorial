05a328a41476ea9c7406231bcc9f5e8f5369edcc
byeonghyeon jeon <pensieve92@gmail.com>
2020-04-17 14:40:28 +0900

    createContext, useContext hook 1

--- hooks-totorial/src/App.js
+++ hooks-totorial/src/App.js
@@ -1,28 +1,29 @@
 import React, { useState } from 'react';
 import Counter from  './Counter';
 import Info from './Info';
+import ContextSample from './ContextSample';
+
+
 
 
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
+  // const [visible, setVisible] = useState(false);
   // return <Counter />;
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? '숨기기': '보이기'}
-      </button>
-      <hr />
-      {visible && <Info/>}
-    </div>    
-  )
-
-  
-  
+  return <ContextSample />
+  // (
+  //   <div>
+  //     <button
+  //       onClick={() => {
+  //         setVisible(!visible);
+  //       }}
+  //     >
+  //       {visible ? '숨기기': '보이기'}
+  //     </button>
+  //     <hr />
+  //     {visible && <Info/>}
+  //   </div>    
+  // )
 }
 
 export default App;
