cf78f4bdc5777d080ea9322652d9e92ea70faa61
JeonBH <pensieve92@gmail.com>
2020-04-19 18:59:31 +0900

    useMemo hook 1

--- hooks-totorial/src/App.js
+++ hooks-totorial/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Counter from  './Counter';
 import Info from './Info';
 import ContextSample from './ContextSample';
+import Average from './Average';
 
 
 
@@ -11,7 +12,9 @@ const App = () => {
   // const [visible, setVisible] = useState(false);
   // return <Counter />;
   // return <ContextSample />
-  return <Info />
+  // return <Info />
+
+  return <Average />
 
   // (
   //   <div>
